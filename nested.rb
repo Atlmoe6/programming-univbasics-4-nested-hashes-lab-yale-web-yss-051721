586c07e614b390c093ff39ebd58edcaf978a5877
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-06-03 17:07:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,7 +74,8 @@ def adding_matz
       }
     }
     programmer_hash[:yukihiro_matsumoto] = {}
-    programmer_hash[:yukihiro_matsumoto][:known_for] = 
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:known_for]
 
 end
 
